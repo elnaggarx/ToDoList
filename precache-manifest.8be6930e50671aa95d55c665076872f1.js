1732d08bc7413c9181e7110d1b97189787b8a8f8
Mohamed Amr <mohamedamr2014.ma@gmail.com>
2024-03-13 22:49:31 +0200

    Updates

---
+++ precache-manifest.8be6930e50671aa95d55c665076872f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80b4cce7329f18b315b9ddbe696f2ee0",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "184a8e2bf4df0fac884e",
+    "url": "/todolist/static/js/2.5ce82d7f.chunk.js"
+  },
+  {
+    "revision": "def00269654d780291b1",
+    "url": "/todolist/static/js/main.6b587ae3.chunk.js"
+  },
+  {
+    "revision": "5225356577f332e6df46",
+    "url": "/todolist/static/js/runtime~main.d3bf418b.js"
+  },
+  {
+    "revision": "17231c5da1a8f2c92de4179efbb91484",
+    "url": "/todolist/static/media/me.17231c5d.jpeg"
+  }
+]);
